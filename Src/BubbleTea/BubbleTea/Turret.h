5508eb2de29e84b618296256f9cae54b21e138ba
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2024-03-29 10:05:53 +0100

    Reorganized classes

---
+++ Src/BubbleTea/BubbleTea/Turret.h
@@ -0,0 +1,12 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+#pragma once
+class Turret
+{
+};
+
--- Src/BubbleTea/BubbleTea/main.cpp
+++ Src/BubbleTea/BubbleTea/main.cpp
@@ -1,6 +1,7 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
 #include <string>
+#include "Bubble.h"
 
 using namespace std;
 using namespace sf;
@@ -23,24 +24,7 @@ void shotBall() {
     
 }
 
-class Bubble : public CircleShape
-{
-    public:
-        float bubbleRadius = 10.f;
-        float thickness = 5.f;
 
-        Vector2f bubblePosition;
-        const Color fillColor;
-        const Color outlineColor = Color::Blue;
-
-        // Constructor for ShotBubble
-        Bubble(Color f, float x, float y) : fillColor(f), bubblePosition(x,y) {
-            setRadius(bubbleRadius); // Set the radius of the circle shape
-            setFillColor(f); // Set the fill color of the circle shape
-            setPosition(bubblePosition);
-        }
-
-};
 
 
 
